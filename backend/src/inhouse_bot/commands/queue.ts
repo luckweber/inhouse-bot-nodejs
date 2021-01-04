d4e744e4772736c5d85f276baaa755b350fe6837
unknown <thiago.pereira@agencian1.com.br>
2021-01-04 19:01:21 -0300

    fix file .env

---
+++ backend/src/inhouse_bot/commands/queue.ts
@@ -0,0 +1,32 @@
+const ROLES = ['top', 'jungle', 'mid', 'adc', 'sup']
+import {isEmpty} from 'ramda'
+
+
+export const queue = async (client:any, message:any, args:any) => {
+
+    const user = message.author
+    const users_duo_size = message.mentions.users.size
+    const users_duo = message.mentions.users
+
+    if(users_duo_size) return queue_duo(user, users_duo, args)
+    else return queue_solo(user, args, message)
+    
+}
+
+const queue_solo = (user:any, args:any, message:any) => {
+
+    if(isEmpty(args)) return message.channel.send(`Role not included`)
+    
+    args = args[0]
+    if(!ROLES.includes(args)) return message.channel.send(`Invalid Role`)
+
+    
+    
+    
+    return message.channel.send(`You are included in the Queue`)
+} 
+
+const queue_duo = (user:any, users:any, args:any) => {
+    
+    
+} 
--- backend/src/inhouse_bot/events/message.ts
+++ backend/src/inhouse_bot/events/message.ts
@@ -1,9 +1,34 @@
+import { readdirSync } from "fs";
+import {envs} from "./../../config"
+const path = require('path'); 
+
+
 export const message = async (client:any, message:any) => {
     
     if(message.author.bot) return
+
+    if (message.content.indexOf(envs.PREFIX) !== 0) return
+
+    const args = message.content.slice(envs.PREFIX.length).trim().split(/ +/g)
+    const command = args.shift().toLowerCase()
+
+    const filesDir = path.resolve(__dirname, './../commands/')
+    const evtFiles = readdirSync(filesDir)
+
+    const file = evtFiles.find((name) => name.split('.')[0] == command)   
     
-    console.log(message.channel.id);
-    console.log(message.guild.roles);
-    
+    if(file) {
+
+        const eventName = file.split('.')[0]
+        let fileCommand =  `${filesDir}/${file}`
+        let command = require(fileCommand)
+        
+        command = command[`${eventName}`]
+        command(client, message, args)
+        
+
+    }else {
+        message.channel.send(`Command ${command} invalid`)
+    }
     
 }
